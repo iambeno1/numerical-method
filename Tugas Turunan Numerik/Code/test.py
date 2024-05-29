4670ed0f7947d5958aa0721af45b125bfa8da096
iambeno1 <benonygabriel40@gmail.com>
2024-05-29 12:09:16 +0700

    perbaiki rumus

--- Tugas Turunan Numerik/Code/test.py
+++ Tugas Turunan Numerik/Code/test.py
@@ -1,49 +1,39 @@
 import numpy as np
 
-# Soal 1
-def f1(x):
-    return np.sin(0.5 * np.sqrt(x)) / x
-
+# Latihan 1: f(x) = sin(0.5)
+f1 = lambda x: np.sin(0.5)
 x1 = 1
 h1 = 0.2
 
-# Selisih Maju
+# Turunan pertama dengan metode selisih maju, mundur, dan pusat
 f1_forward_diff = (f1(x1 + h1) - f1(x1)) / h1
-
-# Selisih Mundur
 f1_backward_diff = (f1(x1) - f1(x1 - h1)) / h1
-
-# Selisih Pusat
 f1_central_diff = (f1(x1 + h1) - f1(x1 - h1)) / (2 * h1)
 
-print("Soal 1:")
-print(f"Selisih Maju: {f1_forward_diff}")
-print(f"Selisih Mundur: {f1_backward_diff}")
-print(f"Selisih Pusat: {f1_central_diff}")
+# Output untuk Latihan 1
+print("Latihan 1:")
+print("Selisih Maju:", f1_forward_diff)
+print("Selisih Mundur:", f1_backward_diff)
+print("Selisih Pusat:", f1_central_diff)
 
-# Soal 2
-def f2(x):
-    return x**2 * np.cos(x)
-
+# Latihan 2: f(x) = x^2 * cos(a)
+a = np.pi / 4  # contoh nilai a
+f2 = lambda x: x**2 * np.cos(a)
 x2 = 0.4
 h2 = 0.1
 
-# Selisih Maju
-f2_forward_diff2 = (f2(x2 + 2*h2) - 2 * f2(x2 + h2) + f2(x2)) / h2**2
+# Turunan kedua dengan metode selisih maju, mundur, dan pusat
+f2_forward_diff2 = (f2(x2 + 2*h2) - 2*f2(x2 + h2) + f2(x2)) / h2**2
+f2_backward_diff2 = (f2(x2) - 2*f2(x2 - h2) + f2(x2 - 2*h2)) / h2**2
+f2_central_diff2 = (f2(x2 + h2) - 2*f2(x2) + f2(x2 - h2)) / h2**2
 
-# Selisih Mundur
-f2_backward_diff2 = (f2(x2) - 2 * f2(x2 - h2) + f2(x2 - 2*h2)) / h2**2
+# Output untuk Latihan 2
+print("\nLatihan 2:")
+print("Selisih Maju:", f2_forward_diff2)
+print("Selisih Mundur:", f2_backward_diff2)
+print("Selisih Pusat:", f2_central_diff2)
 
-# Selisih Pusat
-f2_central_diff2 = (f2(x2 + h2) - 2 * f2(x2) + f2(x2 - h2)) / h2**2
-
-print("\nSoal 2:")
-print(f"Selisih Maju: {f2_forward_diff2}")
-print(f"Selisih Mundur: {f2_backward_diff2}")
-print(f"Selisih Pusat: {f2_central_diff2}")
-
-# Soal 3
-# Data jarak tempuh kendaraan:
+# Latihan 3: Data jarak tempuh kendaraan
 t = np.array([0, 25, 50, 75, 100, 125])
 y = np.array([0, 32, 58, 78, 92, 100])
 
@@ -61,7 +51,7 @@ a[-1] = (y[-1] - 2*y[-2] + y[-3]) / ((t[1] - t[0])**2)  # Selisih mundur untuk t
 for i in range(1, len(t) - 1):
     a[i] = (y[i+1] - 2*y[i] + y[i-1]) / ((t[1] - t[0])**2)  # Selisih pusat untuk titik dalam
 
-# Output soal 3
+# Output untuk Latihan 3
 print("\nLatihan 3:")
 print("Waktu (s)   Kecepatan (km/s)   Percepatan (km/s^2)")
 for i in range(len(t)):
